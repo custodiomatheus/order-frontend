62c0317d3a63ca2da04f42201837c7a48175c542
custodiomatheus <matheuscustodio76@gmail.com>
2025-08-23 13:05:08 -0300

    feat: add tailwindcss

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [vue(), vueDevTools(), tailwindcss()],
   server: { port: 3000 },
   resolve: {
     alias: {
