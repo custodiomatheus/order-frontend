c25d24825a156a612aed7de5fe910a07c544315d
custodiomatheus <matheuscustodio76@gmail.com>
2025-08-25 23:57:43 -0300

    feat: create home

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <main class="flex w-full h-[calc(100vh-4rem)] items-center justify-center">
+    <router-link
+      :to="{ name: 'orders.details', params: { id: 1 } }"
+      class="btn bg-primary-500 text-white p-2 rounded-lg hover:opacity-80 transition-opacity ease-in-out"
+    >
+      Ver ordem
+    </router-link>
+  </main>
+</template>
