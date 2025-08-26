cc22fe2c0da09eee56f410b18048f9658cb26f74
custodiomatheus <matheuscustodio76@gmail.com>
2025-08-25 23:59:21 -0300

    feat: orderaddress component

---
+++ src/components/Order/OrderAddress.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ChevronUp } from 'lucide-vue-next'
+import OrderCard from './OrderCard.vue'
+
+defineProps({
+  addresses: { type: Array, default: () => [] },
+})
+</script>
+
+<template>
+  <div>
+    <h4 class="text-neutral-700 text-lg font-bold flex items-center gap-2">
+      <ChevronUp :size="20" color="#1052e0" fill="#edf3ff" />
+      Addresses
+    </h4>
+
+    <div class="flex flex-col gap-4 lg:flex-row">
+      <OrderCard
+        v-for="address in addresses"
+        :key="address.label"
+        :label="address.label"
+        :name="address.name"
+        :code="address.code"
+        :address="address.address"
+        :contact="address.contact"
+      />
+    </div>
+  </div>
+</template>
